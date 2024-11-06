630bca7c97f34633a05807fbbb211ab0e8c0ee5b
Liam Eckert <liameckert17@gmail.com>
2024-11-06 08:05:17 -0500

    reworking the color theme

---
+++ client/src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css';
+
+export default function Header(): JSX.Element {
+    // TODO: Add nav links
+    return (
+        <header>
+            <h1>Microblog</h1>
+        </header>
+    )
+}

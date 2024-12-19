70d999c839735d197916d94172b579c22c91eb66
Liam Eckert <liameckert17@gmail.com>
2024-12-19 05:55:54 -0500

    Working dotenv and basic backend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,15 +1,35 @@
-#[macro_use] extern crate rocket;
+use std::env;
 
-#[get("/")]
-fn index() -> &'static str {
+use axum::{
+    routing::get,
+    Router,
+};
+
+async fn hello_world() -> &'static str {
     "Hello, world!"
 }
 
-#[rocket::main]
-async fn main() -> Result<(), rocket::Error> {
-    let _rocket = rocket::build()
-        .mount("/", routes![index])
-        .launch()
-        .await?;
-    Ok(())
+#[tokio::main]
+async fn main() {
+    dotenvy::dotenv().ok();
+
+    let address: String;
+    let port: String;
+
+    #[cfg(feature = "prod")]
+    {
+        address = env::var("PROD_ADDRESS").unwrap();
+        port = env::var("PROD_PORT").unwrap();
+    }
+    #[cfg(not(feature = "prod"))]
+    {
+        address = env::var("DEV_ADDRESS").unwrap();
+        port = env::var("DEV_PORT").unwrap();
+    }
+
+    let app = Router::new()
+        .route("/", get(hello_world));
+
+    let listener = tokio::net::TcpListener::bind(format!("{}:{}", address, port)).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
